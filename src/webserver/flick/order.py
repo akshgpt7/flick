1de1120d393923ae65559e80b7487deb1abee2c1
gracewgao <ggaoww@gmail.com>
2020-10-13 03:27:13 -0400

    Updated to use GET requests in CLI instead of POST for displaying information

--- src/webserver/flick/order.py
+++ src/webserver/flick/order.py
@@ -4,7 +4,6 @@ from flask import (
 from werkzeug.exceptions import abort
 from .db import get_db
 from .order_queues import queues
-import json
 
 bp = Blueprint('order', __name__)
 
@@ -12,28 +11,8 @@ bp = Blueprint('order', __name__)
 @bp.route('/order', methods=['GET', 'POST',])
 def order():
     if request.method == 'POST':
-
-        # Retrieves JSON request and removes extra bracket
-        order_request = json.loads(request.get_json(force=True))[0]
-        order_details = json.dumps(order_request)
-
-        joint_id = order_details['joint_id']
-        pizza_id = order_details['order_details']['item_id']
-        size = order_details['order_details']['size']
-
-        db = get_db()
-        details = db.execute(
-            'SELECT name, toppings, ?'
-            ' FROM pizzas i JOIN prices p ON i.pizza_id = r.pizza_id'
-            ' WHERE i.pizza_id = ?',
-            (size, pizza_id,)
-        ).fetchone()
-
-        details.append(db.execute(
-            'SELECT name FROM joints'
-            ' WHERE joint_id = ?',
-            (joint_id,)
-        ).fetchone())
+        order_details = request.get_json(force=True)
+        joint_id = order_details.get('joint_id')
 
         if joint_id in queues.keys():
             message = "Order placed successfully!"
@@ -44,17 +23,7 @@ def order():
         joint_order_queue = queues.get(joint_id)
         joint_order_queue.append(order_details)
 
-        response_json = []
-
-        if details is not None:
-            response_json = {'pizza_name': order_details[0], 'toppings': order_details[1],
-                            'price': order_details[2], 'joint_name': order_details[3],
-                            'order_status': f'{message}'
-                        }
-
-        return jsonify(response_json)
-
-        # return jsonify({'order_status': f'{message}'})
+        return jsonify({'order_status': f'{message}'})
 
     else:
         return str(queues)
